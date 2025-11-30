a76e2d353a49400ff0ccbf70b7e9c2c222fda26f
shubham452 <55233300+shubham452@users.noreply.github.com>
2025-11-30 13:11:52 +0530

    Update and rename a.txt to notes.cpp

---
+++ binary search/upper bound lower bound/notes.cpp
@@ -0,0 +1,4 @@
+when finding upper bound we try to move left to mid + 1 and then in else right gets mid because we want strictly larger than target
+
+and 
+in lower bound since we want 1st that match target we move right to mid-1 and in else left=mid because we want the first match
